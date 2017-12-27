6e5707377be60be012e65bc3a37651e7561c1457
Ismar Sacirovic <ismarguitar@gmail.com>
2017-12-27 18:27:32 +0100

    Adds some comments

--- One_Liners.py
+++ One_Liners.py
@@ -3,11 +3,14 @@ def word_lengths(phrase):
 
 print(list(word_lengths('How long are the words in this phrase!! Halleluyaa')))
 
+'''
+Lambda function was not needed because we have a built-in function to check the Length.
+Fuction name is len(), and we do not write the () in map: >map(len, phrase.slit()<
+'''
+
 from functools import reduce
-
-
 def digits_to_num(digits):
-    return reduce(lambda x, y,: (x *10 + y), digits)
+    return reduce(lambda x, y,: x *10 + y, digits)
 
 print (digits_to_num([3,4,3,2,1]))
 
@@ -16,4 +19,9 @@ def filter_words(w, l):
 
 lista = ['hey','geyx','sexy','hamburger']
 print (list((filter_words(lista, 'h'))))
+
+def concatenate(L1, L2, connector):
+    return [word1+connector+word2 for (word1, word2) in zip(L1,L2)]
+
+print (concatenate(['A','B'],['a','b'],'-'))
 
