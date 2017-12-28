913d756a8faeefce3620a4e4782323574e575360
Ismar Sacirovic <ismarguitar@gmail.com>
2017-12-28 17:03:13 +0100

    Adds Break and Continue

---
+++ docs.python.org/04_04_Break_Continue.py
@@ -0,0 +1,21 @@
+print('''\
+Loop statements may have an else clause; it is executed when the loop terminates through exhaustion of the list (with for) 
+or when the condition becomes false (with while), but not when the loop is terminated by a break statement. 
+This is exemplified by the following loop, which searches for prime numbers:)
+''')
+
+for n in range(2, 10):
+    for x in range(2, n):
+       if n % x == 0:
+            print (n, 'equals', x, '*', int(n/x))
+            break
+    else:
+        #loop fell through without finding a factor
+        print (n, 'is a prime number')
+
+
+for num in range(2, 10):
+    if num % 2 == 0:
+        print ("Found an even number", num)
+        continue
+    print ("Found a number", num)
