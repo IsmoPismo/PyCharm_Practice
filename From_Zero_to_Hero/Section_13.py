00e602d622c89b2808f04ecae9057557bbc63c17
Ismar Sacirovic <ismarguitar@gmail.com>
2017-12-29 16:40:50 +0100

    Adds Section 13 Part2

--- From_Zero_to_Hero/Section_13.py
+++ From_Zero_to_Hero/Section_13.py
@@ -19,4 +19,23 @@ print(pozz)
 
 #Decorators part2
 
+def hoi(name='Ismar'):
+    print ("Hoi() Executed")
+    def cao():
+        return ('''\
+        Ovo je cao\
+            ''')
+    def welcome():
+        return ('''\
+        Wellcome xD\
+        ''')
 
+    print(cao())
+    print(welcome())
+    print ('Sad smo vanka u hio()')
+
+
+hoi()
+y = hoi
+y()
+print(y)
