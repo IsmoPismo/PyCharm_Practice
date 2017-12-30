480ccf8523ebfc673f0335fe0565e2703d7d4832
Ismar Sacirovic <ismarguitar@gmail.com>
2017-12-30 13:14:33 +0100

    Adds Section 13 Part 4

--- From_Zero_to_Hero/Section_13.py
+++ From_Zero_to_Hero/Section_13.py
@@ -1,4 +1,4 @@
-#Decorators part1
+print('Decorators part1')#Decorators part1
 
 s = "I'm global!"
 def func():
@@ -17,7 +17,7 @@ del hello
 print(pozz)
 
 
-#Decorators part2
+print('\nDecorators part2')#Decorators part2
 
 def hoi(name='Ismar'):
     print ("Hoi() Executed")
@@ -37,5 +37,60 @@ def hoi(name='Ismar'):
 
 hoi()
 y = hoi
-y()
+print('Bez zagrade je SAMA funkcija, nije EXEcuted')
 print(y)
+
+
+print('\nDecorators part3')#Decorators part3
+
+def hello(name='Jose'):
+
+    def greet():
+        return '\t This is inside the greet() function'
+
+    def welcome():
+        return "\t This is inside the welcome() function"
+
+    if name == 'Jose':
+        return greet
+    else:
+        return welcome
+
+x=hello()
+print(x)
+
+
+print('\nDecorators part4')#Decorators part4
+
+def fukcija():
+    return ('Jebeš mu mater')
+
+def funkcija2():
+    return ('Jebi mu mamu!')
+
+def druga_funkcija(fank):
+    print('Ova funkcija printa drugu funkciju: ')
+    return(fank())
+
+print (druga_funkcija(fukcija))
+print (druga_funkcija(fukcija))
+
+
+print('\nCreating a Decorator') #Creating a Decorator
+
+def dekorator(fukcionalnost):
+    def wrap():
+        print ('Ovo se prvo vidi')
+        fukcionalnost()
+        print('Ovo je poslije')
+
+    return wrap()
+
+def fukcija_za_decorisat():
+    print ('Meni treba decorator')
+
+@dekorator
+def fukcija_za_decorisat():
+    print ('Meni treba decorator')
+
+print (fukcija_za_decorisat)
