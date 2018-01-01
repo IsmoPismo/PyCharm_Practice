2ca5bd617c6ee4d7d151ccb4a5333589f0bac764
Ismar Sacirovic <ismarguitar@gmail.com>
2018-01-01 23:43:39 +0100

    Adds Nested List Comprehensions

---
+++ docs.python.org/05_Data_Structures/5_1_41_Nested_List_Comprehension.py
@@ -0,0 +1,36 @@
+matrix = [
+[1, 2, 3, 4],
+[5, 6, 7, 8],
+[9, 10, 11, 12],
+]
+
+print([[row[i] for row in matrix] for i in range(4)])
+
+
+transposed = []
+for i in range(4):
+    transposed.append([row[i] for row in matrix])
+
+print (transposed)
+
+
+re_transposed = list()
+for i in range(3):
+    re_transposed_row = list()
+    for row in transposed:
+        re_transposed_row.append(row[i])
+    re_transposed.append(re_transposed_row)
+
+print(re_transposed)
+
+print ([[row[i] * 9 for row in re_transposed if row[i] * 9 > 20] for i in range(4)])
+
+print('''
+        When working with nested loops in list comprehensions remember 
+        that the for clauses remain 
+        in the same order as in our original for loops.
+    ''')
+
+
+for x in range(2):
+    print (x*3)
